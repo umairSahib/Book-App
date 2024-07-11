8b29ebc7331f9a937f519dfc67c250e3d3498f02
Your Name <your_email@example.com>
2024-07-11 21:06:36 +0500

    new

--- src/component/Cards.jsx
+++ src/component/Cards.jsx
@@ -15,7 +15,7 @@ const Cards = ({ books }) => {
               <div className="bg-white rounded-lg border p-4">
                 <div className="text">
                   <img
-                    src={`https://covers.openlibrary.org/b/isbn/${book.isbn[1]}.jpg`}
+                    src={`https://covers.openlibrary.org/b/isbn/${book.isbn[0]}.jpg`}
                     alt="card "
                     className="w-full h-48 rounded-md object-cover"
                   />
@@ -37,47 +37,3 @@ const Cards = ({ books }) => {
 };
 
 export default Cards;
-
-// <div className=" flex-items-center grid " key={index}>
-// <div className="bg-white rounded-lg border p-4">
-//   <img
-//     src={object.img}
-//     alt="/"
-//     className="w-full h-48 rounded-md object-cover"
-//   />
-//   <div className="px-1 py-4">
-//     <div className="font-bold text-xl mb-2">{object.name}</div>
-
-//     <p className="text-gray-700 text-base">A very Healthy & Fresh</p>
-//     <p className="font-bold text-xl mb-2">{object.price}</p>
-//   </div>
-//   <div className="px-1 py-4">
-//     <div className="inline-flex">
-//       <button
-//         onClick={() => props.increment(object, index)}
-//         className="bg-gray-300 hover:bg-gray-400 text-gray-800 font-bold py-2 px-4 rounded-r"
-//       >
-//         +
-//       </button>
-//       <button className="bg-gray-300 hover:bg-gray-400 text-gray-800 font-bold py-2 px-4 rounded-r">
-//         {object.quantity}
-//       </button>
-//       <button
-//         disabled={object.quantity === 0 ? true : false}
-//         onClick={() => props.decrement(object, index)}
-//         className="bg-gray-300 hover:bg-gray-400 text-gray-800 font-bold py-2 px-4 rounded-r"
-//       >
-//         -
-//       </button>
-//     </div>
-//     <div className="btn">
-//       <button
-//         onClick={() => props.addToCart(object)}
-//         className="bg-blue-300 hover:bg-gray-400 text-gray-800 font-bold py-2 px-4 rounded-r my-2 justify-center"
-//       >
-//         Add To Cart
-//       </button>
-//     </div>
-//   </div>
-// </div>
-// </div>
